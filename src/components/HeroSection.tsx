8c34a5dc71a74cf7adc4b9c85d72be61672c1acf
leeya018 <leeyahav018@gmail.com>
2024-05-03 12:49:30 +0300

    s

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,57 +4,56 @@ import Image from "next/image";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="relative text-white text-center">
+    <div className="relative bg-purple-700 flex text-white text-center flex-col ">
       {/* Background and Overlay */}
-      <div className="bg-purple-700 absolute inset-0 z-0"></div>
+      {/* <div className="bg-purple-700 absolute inset-0 z-0"></div> */}
 
-      {/* Content */}
-      <div className="relative z-10 flex  items-center justify-center h-screen">
-        {/* Profile Image */}
+      {/* Tagline */}
+      <div className=" flex flex-col items-center text-white font-bold py-10 mx-auto ">
         <Image
-          src="https://images.unsplash.com/photo-1559028012-481c04fa702d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          alt="John Smith"
-          className=" mb-4 border-4 border-white w-[10rem] h-[22rem] object-fill bg-center rounded-md"
+          src="https://images.unsplash.com/profile-fb-1611877906-90959b365216.jpg?bg=fff&crop=faces&dpr=1&h=150&w=150&auto=format&fit=crop&q=60&ixlib=rb-4.0.3"
+          alt="Lee Yahav"
+          className="rounded-full mb-4 border-4 border-white w-44 h-44 "
           width={96} // width in pixels
           height={96} // height in pixels
           layout="fixed"
         />
-
-        {/* Tagline */}
-        <div className="flex flex-col items-center text-white font-bold ">
-          <Image
-            src="https://images.unsplash.com/photo-1559028012-481c04fa702d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="John Smith"
-            className="rounded-full mb-4 border-4 border-white"
-            width={96} // width in pixels
-            height={96} // height in pixels
-            layout="fixed"
-          />
-          <h1 className="text-4xl font-bold mb-2">Thinking for Creativity</h1>
-          <p className="text-xl">John Smith</p>
-          <p className="text-base font-light">Webflow Developer</p>
-        </div>
-
-        {/* Additional UI Elements like the mobile app mockup */}
-        <div className="mt-8">
-          {/* <img
-            src="/path-to-app-mockup.png"
-            alt="Mobile App Mockup"
-            className="w-40 h-auto mx-auto"
-          /> */}
-          <Image
-            src="https://images.unsplash.com/photo-1559028012-481c04fa702d?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="John Smith"
-            className="rounded-full mb-4 border-4 border-white"
-            width={96} // width in pixels
-            height={96} // height in pixels
-            layout="fixed"
-          />
-          <p className="text-white mt-4">Moxa Template</p>
-        </div>
+        <h1 className="text-4xl font-bold mb-2">Thinking for Creativity</h1>
+        <p className="text-xl">Lee Yahav</p>
+        <p className="text-base font-light">Web Developer</p>
+      </div>
+      {/* icons  */}
+      <div className=" flex items-center justify-between mx-auto gap-10 text-blue-500 ">
+        <Icon />
+        <Icon />
+        <Icon />
+        <Icon />
+        <Icon />
       </div>
     </div>
   );
 };
 
 export default HeroSection;
+
+function Icon() {
+  return (
+    <>
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="-10.5 -9.45 21 18.9"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className="uwu-hidden mt-4 mb-3 text-brand dark:text-brand-dark w-24 lg:w-28 self-center text-sm me-0 flex origin-center transition-all ease-in-out hover:rotate-180 duration-1000"
+      >
+        <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
+        <g stroke="currentColor" stroke-width="1" fill="none">
+          <ellipse rx="10" ry="4.5"></ellipse>
+          <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
+          <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
+        </g>
+      </svg>
+    </>
+  );
+}
