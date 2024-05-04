d9619228a4bea255d8bcbdd75ec0413e772de043
leeya018 <leeyahav018@gmail.com>
2024-05-04 19:54:16 +0300

    pricing

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import PricingSection from "@/components/PricingSection";
+
+const PricingPage = () => {
+  return (
+    <>
+      <PricingSection />
+    </>
+  );
+};
+
+export default PricingPage;
